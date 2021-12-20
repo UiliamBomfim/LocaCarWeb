284103965bdb403dbfaeaeb7a1213bdf270fbf62
Davi Rocha Macedo Sousa <davirmsousa@hotmail.com>
2021-12-20 08:59:51 -0300

    atualizando base url

--- frontend/src/services/BaseApi.js
+++ frontend/src/services/BaseApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BaseApi = () => {
     
-    const BASE_URL = "http://127.0.0.1:8000/"
+    const BASE_URL = "http://ubomfim.pythonanywhere.com/"
 
     const defaultConfig = {
         "Content-Type": "application/json",
